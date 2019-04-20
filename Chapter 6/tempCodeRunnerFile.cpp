738e116397c6e6f0f0d2576ca6c78f8b62ddce48
Yannik Eckert <yannik.eckert@uni-oldenburg.de>
2019-04-20 22:43:10 +0200

    added things and changed gitignore

--- Chapter 6/tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-(sentence() && conjunction() && sentence());
-	// return (b1
--- Chapter 7/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-Token t2 = ts.get();
-        //Handle Assignment operation
-        if (t2.kind == assignment)
-        {
-            double d = expression();
-            set_value(t.name, d);
-            return d;
-        }
