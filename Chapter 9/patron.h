30ebdcebfb4ea214d32e6cf059869ee2cc24e661
Yannik Eckert <yannik.eckert@gmx.de>
2019-10-06 15:18:41 +0900

    Changed clang-format and formated patron.h

--- Chapter 9/patron.h
+++ Chapter 9/patron.h
@@ -4,13 +4,13 @@ namespace Patron
 {
 class Patron
 {
-  string surname;
-  string prename;
-  int    card_number;
-  double library_fees;
+    string surname;
+    string prename;
+    int    card_number;
+    double library_fees;
 
   public:
-  Patron();
-  Patron(string, string, int, double);
+    Patron();
+    Patron(string, string, int, double);
 };
 }  // namespace Patron
