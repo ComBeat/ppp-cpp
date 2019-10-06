076e492b901ee99d49f304187bf286be876361a0
Yannik Eckert <yannik.eckert@gmx.de>
2019-10-06 18:16:49 +0900

    Finished Ex08

---
+++ Chapter 9/patron.cpp
@@ -0,0 +1,23 @@
+#include "patron.h"
+
+namespace Patron
+{
+Patron::Patron()
+    : surname{""}, prename{""}, card_number{0}, library_fees{0.0} {
+}
+
+Patron::Patron(string sname, string pname, int cnumber, double fees)
+    : surname{sname}, prename{pname}, card_number{cnumber} {
+    if (fees >= 0.0)
+        library_fees = fees;
+    else
+        error("Library fees can't be negative");
+}
+
+bool Patron::owes_fee() {
+    if (library_fees > 0.0)
+        return true;
+    else
+        return false;
+}
+}  // namespace Patron
--- Chapter 9/patron.h
+++ Chapter 9/patron.h
@@ -12,5 +12,15 @@ class Patron
   public:
     Patron();
     Patron(string, string, int, double);
+
+    bool owes_fee();
+
+    string get_surname() { return surname; }
+    void   set_surname(string sname) { surname = sname; }
+    string get_prename() { return prename; }
+    void   set_prename(string pname) { prename = pname; }
+    int    get_card_number() { return card_number; }
+    double get_library_fees() { return library_fees; }
+    void   set_library_fees(double fees) { library_fees = fees; }
 };
 }  // namespace Patron
